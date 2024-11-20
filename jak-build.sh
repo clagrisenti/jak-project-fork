32ddc16f31d955cff07d75a317bfb051bdddf9d0
cla.grisenti <clagrr@gmail.com>
2024-11-20 21:32:54 +0100

    Fix assert

--- jak-build.sh
+++ jak-build.sh
@@ -8,13 +8,21 @@ buildModes=("Release")
 for mode in ${buildModes[@]}; do
 
     buildDir="build"
+    assert=""
 
-    if [ "$1" == "clean" ]; then
-        echo "cleaning build"
-        cmake --build $buildDir --target clean
-    fi
+    for var in "$@"; do
+        if [ "$var" == "clean" ]; then
+            echo "cleaning build"
+            cmake --build $buildDir --target clean
+        fi
+
+        if [ "$var" == "no_assert" ]; then
+            echo "disabling custom asserts"
+            assert="-DCMAKE_CXX_FLAGS=\"-DNO_ASSERT=1\""
+        fi
+    done
 
     echo "building $mode"
-    cmake -B $buildDir -DCMAKE_BUILD_TYPE=$mode && cmake --build $buildDir -j 13
+    cmake -B $buildDir -DCMAKE_BUILD_TYPE=$mode $assert && cmake --build $buildDir -j 15
 
 done
